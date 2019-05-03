dfee0b9f7b5976d1fe150ae1c4984f0270ba2576
rmaghsoudi <romy_mag@outlook.com>
2019-05-02 21:35:23 -0400

    Done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,47 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@artists = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@artists << self
+  end
+
+  def self.all
+    @@artists
+  end
+
+  def paintings
+    Painting.all.select { |painting| painting.artist == self }
+  end
+
+  def galleries
+    self.paintings.map { |painting| painting.gallery }
+  end
+
+  def cities
+    self.paintings.map { |painting| painting.gallery.city }
+  end
+
+  def self.total_experience
+    total = 0
+    @@artists.each { |artist| total += artist.years_experience }
+    total
+  end
+
+  def self.most_prolific
+    experience = @@artists.map { |artist| artist.years_experience }
+    indexed_paintings = @@artists.map { |artist| Painting.all.select { |painting| painting.artist == artist } }
+    painting_count = indexed_paintings.map { |array| array.count }
+    values = painting_count.each_with_index{ |number, index| number.to_f / experience[index] }
+    the_index = values.index(values.max)
+    @@artists[the_index]
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,14 @@
 require_relative '../config/environment.rb'
 
+jojo = Artist.new("Jojo", 20)
+dio = Artist.new("Dio", 1)
+louvre = Gallery.new("The Louvre", "Paris")
+phx = Gallery.new("Phoenix Art", "Phoenix")
+nut = Painting.new("A nut", 5000, jojo, louvre)
+bold = Painting.new("Bold nuts", 2000, jojo, phx)
+public = Painting.new("Marta Train", 1000, dio, louvre)
+pub = Painting.new("Train", 2500, dio, louvre)
+lic = Painting.new("Marta", 95, dio, louvre)
 
 
 
